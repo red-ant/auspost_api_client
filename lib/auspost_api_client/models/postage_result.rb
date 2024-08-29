c09bb8e87dec536a4b958c4a15c8a46e1fc1eb43
Mike Causer <mcauser@gmail.com>
2024-08-29 13:34:09 +1000

    Add delivery_time to PostageResult

--- lib/auspost_api_client/models/postage_result.rb
+++ lib/auspost_api_client/models/postage_result.rb
@@ -1,15 +1,19 @@
 module AuspostApiClient
-    module Models
-        class PostageResult
-            attr_accessor :service, :total_cost, :costs
-            def self.from_hash(hash)
-                ret = PostageResult.new
-                new_hash = hash[:postage_result]
-                ret.service = new_hash[:service]
-                ret.total_cost = new_hash[:total_cost]
-                ret.costs = new_hash[:costs]
-                ret
-            end
-        end
+  module Models
+    class PostageResult
+      attr_accessor :service, :delivery_time, :total_cost, :costs
+
+      def self.from_hash(hash)
+        res = PostageResult.new
+        new_hash = hash[:postage_result]
+
+        res.service = new_hash[:service]
+        res.delivery_time = new_hash[:delivery_time]
+        res.total_cost = new_hash[:total_cost]
+        res.costs = new_hash[:costs]
+
+        res
+      end
     end
+  end
 end
